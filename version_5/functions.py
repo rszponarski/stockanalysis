d6f2f0f1ba269eac2691c9f2f6dc8534dd26318c
rafal <rafal.szponarski@o2.pl>
2024-06-02 18:08:13 +0200

    new version_5 - company selected by the user

---
+++ version_5/functions.py
@@ -0,0 +1,73 @@
+import time
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+from tkinter.messagebox import showerror
+
+
+def stock_market_data(index, start_date, end_date, interval, show_volume=False, show_extremes=False):
+    try:
+        start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+        end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+        start_date_converted = int(time.mktime(start_date.timetuple()))
+        end_date_converted = int(time.mktime(end_date.timetuple()))
+
+        # URL na razie pozostaje bez zmian, ponieważ nie używamy jeszcze parametru 'index'
+        url = (f'https://query1.finance.yahoo.com/v7/finance/download/CDR.WA?'
+               f'period1={start_date_converted}&period2={end_date_converted}&interval={interval}&events=history&includeAdjustedClose=true')
+
+        df = pd.read_csv(url)
+        df['Date'] = pd.to_datetime(df['Date'])
+
+        fig, ax1 = plt.subplots(figsize=(10, 5))
+
+        ax1.plot(df['Date'], df['Close'], label='Close Price', color='blue')
+        ax1.set_xlabel('Date')
+        ax1.set_ylabel('Price, PLN', color='blue')
+        ax1.tick_params('y', colors='blue')
+        ax1.tick_params(axis='x', labelcolor='black')
+
+        if show_volume:
+            ax2 = ax1.twinx()
+            ax2.bar(df['Date'], df['Volume'], color='green', alpha=0.5, label='Volume')
+            ax2.set_ylabel('Volume', color='green')
+            ax2.tick_params('y', colors='green')
+            ax2.tick_params(axis='x', labelcolor='black')
+
+            # Format volume axis labels to avoid scientific notation
+            formatter = FuncFormatter(lambda x, _: f'{int(x):,}')
+            ax2.yaxis.set_major_formatter(formatter)
+
+        if show_extremes:
+            max_value = df['Close'].max()
+            min_value = df['Close'].min()
+            max_date = df.loc[df['Close'].idxmax(), 'Date']
+            min_date = df.loc[df['Close'].idxmin(), 'Date']
+
+            ax1.plot(max_date, max_value, 'ro')  # Max value marker
+            ax1.plot(min_date, min_value, 'ro')  # Min value marker
+
+            ax1.text(max_date, max_value, f'{max_value:.2f} PLN', ha='left', va='bottom')  # Text for max value
+            ax1.text(min_date, min_value, f'{min_value:.2f} PLN', ha='left', va='bottom')  # Text for min value
+
+        plt.title('CD Projekt Red Stock Price')
+        ax1.legend(loc='upper left')
+        if show_volume:
+            ax2.legend(loc='upper left', bbox_to_anchor=(0, 0.92))
+        plt.grid(True)
+
+        plt.show()
+    except Exception as exc:
+        showerror("Error", str(exc))
+
+
+def get_preset_dates(option):
+    end_date = datetime.datetime.now()
+    if option == 1:  # Last week
+        start_date = end_date - datetime.timedelta(weeks=1)
+    elif option == 2:  # Last month
+        start_date = end_date - datetime.timedelta(days=30)
+    elif option == 3:  # Last quarter
+        start_date = end_date - datetime.timedelta(days=90)
+    return start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')
---
+++ version_5/main_program.py
@@ -0,0 +1,99 @@
+from tkinter import Tk, Label, Entry, Button, Checkbutton, StringVar, BooleanVar, Radiobutton, IntVar
+from tkinter import ttk  # Import ttk for Combobox
+from functions import stock_market_data, get_preset_dates
+import datetime
+
+
+def input_data():
+    selected_index = index_var.get()
+    selected_preset = preset_var.get()
+    if selected_preset != 4:  # if not specific date range
+        start_date, end_date = get_preset_dates(selected_preset)
+    else:  # if specific date range
+        start_date = start_date_var.get()
+        end_date = end_date_var.get()
+
+    interval = interval_var.get()
+    stock_market_data(selected_index, start_date, end_date, interval, volume_checkbox_var.get(), max_min_checkbox_var.get())
+
+
+def toggle_date_range_button():
+    if preset_var.get() == 4:
+        show_date_fields()
+    else:
+        hide_date_fields()
+        download_button.grid(row=10, column=0, columnspan=2)  # Move download button to row 10
+
+
+def show_date_fields():
+    start_date_label.grid(row=3, column=2)
+    start_date_entry.grid(row=3, column=3)
+    end_date_label.grid(row=4, column=2)
+    end_date_entry.grid(row=4, column=3)
+
+    download_button.grid(row=10, column=0, columnspan=2)
+
+
+def hide_date_fields():
+    start_date_label.grid_remove()
+    start_date_entry.grid_remove()
+    end_date_label.grid_remove()
+    end_date_entry.grid_remove()
+
+    download_button.grid(row=10, column=0, columnspan=2)
+
+
+root = Tk()
+root.title("Stock Price Data Downloader")
+
+# Section for index selection
+Label(root, text="Select index:", font=("Arial", 11, "bold"), fg="#003366").grid(row=0, column=0, columnspan=2)
+index_var = StringVar(value='WIG20')
+index_combobox = ttk.Combobox(root, textvariable=index_var, values=['WIG20', 'mWIG40', 'sWIG80', 'other'])
+index_combobox.grid(row=0, column=2, columnspan=2)
+
+# Section for preset date range options
+Label(root, text="Select chart range:", font=("Arial", 11, "bold"), fg="#003366").grid(row=1, column=0, columnspan=4)
+preset_var = IntVar(value=2)
+Radiobutton(root, text="Last Week", variable=preset_var, value=1, command=toggle_date_range_button).grid(row=2, column=0)
+Radiobutton(root, text="Last Month", variable=preset_var, value=2, command=toggle_date_range_button).grid(row=2, column=1)
+Radiobutton(root, text="Last Quarter", variable=preset_var, value=3, command=toggle_date_range_button).grid(row=2, column=2)
+Radiobutton(root, text="Select Specific Date Range", variable=preset_var, value=4, command=toggle_date_range_button).grid(row=2, column=3)
+
+# Section for specific date range inputs
+start_date_var = StringVar(value='2024-01-01')
+end_date_var = StringVar(value=datetime.datetime.now().strftime('%Y-%m-%d'))
+
+start_date_label = Label(root, text="Start Date (YYYY-MM-DD):")
+start_date_entry = Entry(root, textvariable=start_date_var)
+
+end_date_label = Label(root, text="End Date (YYYY-MM-DD):")
+end_date_entry = Entry(root, textvariable=end_date_var)
+
+# Section for interval selection
+interval_var = StringVar(value='1d')
+Label(root, text="Frequency:", font=("Arial", 11, "bold"), fg="#003366").grid(row=5, column=0, columnspan=4)
+Radiobutton(root, text="daily", variable=interval_var, value='1d').grid(row=6, column=1, sticky='e')
+Radiobutton(root, text="weekly", variable=interval_var, value='1wk').grid(row=6, column=2)
+Radiobutton(root, text="monthly", variable=interval_var, value='1mo').grid(row=6, column=3, sticky='w')
+
+# Volume checkbox
+volume_checkbox_var = BooleanVar()
+volume_checkbox_var.set(False)
+volume_checkbox = Checkbutton(root, text="Display volume", font=("Arial", 10), fg="#003366", variable=volume_checkbox_var)
+volume_checkbox.grid(row=8, column=0, columnspan=2, sticky='w')
+
+# Max/min value checkbox
+max_min_checkbox_var = BooleanVar()
+max_min_checkbox_var.set(False)
+max_min_checkbox = Checkbutton(root, text="Show max/min value", font=("Arial", 10), fg="#003366", variable=max_min_checkbox_var)
+max_min_checkbox.grid(row=9, column=0, columnspan=2, sticky='w')
+
+# Download button
+download_button = Button(root, text="Download Chart", font=("Arial", 10, "bold"), fg="#003366", command=input_data)
+Label(root, text="").grid(row=11, column=1)  # Additional empty row
+
+# Initially hide date fields and set initial positions
+hide_date_fields()
+
+root.mainloop()
